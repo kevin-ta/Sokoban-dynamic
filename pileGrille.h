dd48f825fca001cddc2d5db9c9848619a0f5c1d8
Kévin Ta <kevin.ta@edu.esiee.fr>
2016-06-13 18:16:25 +0200

    Added comments on functions

--- pileGrille.h
+++ pileGrille.h
@@ -10,10 +10,39 @@ typedef struct _cell {
 } Cell;
 typedef Cell * List;
 
+/**
+ * Crée une nouvelle cellule
+ * @param
+ * @return Cell *
+ */
 Cell * newCell();
+
+/**
+ * Libère la mémoire allouée à une cellule
+ * @param Cell *
+ * @return
+ */
 void freeCell(Cell *);
+
+/**
+ * Libère la mémoire allouée à une liste
+ * @param List
+ * @return
+ */
 void freeList(List);
+
+/**
+ * Ajoute la grille et la position à la liste
+ * @param List *, Grille, Position
+ * @return
+ */
 void push(List *, Grille, Position);
+
+/**
+ * Retire le dernier élément de la liste
+ * @param List *
+ * @return
+ */
 void pop(List *);
 
 #endif
--- sokoban.h
+++ sokoban.h
@@ -23,21 +23,116 @@ typedef struct Position {
     int y;
 } Position;
 
+/**
+ * Affiche la grille par itération
+ * @param Grille
+ * @return
+ */
 void afficheGrille(Grille);
+
+/**
+ * Initie la grille
+ * @param char *
+ * @return Grille
+ */
 Grille initGrille(char *);
+
+/**
+ * Libère la mémoire réservée à la grille
+ * @param Grille
+ * @return
+ */
 void freeGrille(Grille);
+
+/**
+ * Retourne la position du Sokoban sur la grille
+ * @param Grille
+ * @return Position
+ */
 Position getSokoban(Grille);
+
+/**
+ * Affiche l'erreur envoyé en paramètre sous forme verbale
+ * @param CodeErreur
+ * @return
+ */
 void error(CodeErreur);
+
+/**
+ * Crée une grille à afficher et une grille à cacher
+ * @param Grille
+ * @return
+ */
 void nouveauJeu(Grille);
+
+/**
+ * Vérifie si le nombre de caisses est égal au nombre de cibles
+ * @param Grille
+ * @return Boolean
+ */
 Boolean verif(Grille);
+
+/**
+ * Compte le nombre d'occurrence d'un élément dans la grille
+ * @param Grille, char
+ * @return integer
+ */
 int compte(Grille, char);
+
+/**
+ * Affiche l'aide
+ * @param 
+ * @return
+ */
 void aide();
+
+/**
+ * Retourne la future position suite à une commande mouvement
+ * @param Position, Commande
+ * @return Position
+ */
 Position pas(Position, Commande);
+
+/**
+ * Teste si un mouvement est possible en fonction de la grille
+ * @param Grille, Position
+ * @return Boolean
+ */
 Boolean possible(Grille, Position);
-void deplace(Grille,Position,Position, Grille);
+
+/**
+ * Déplace le Sokoban sur la Grille
+ * @param Grille, Position, Position, Grille
+ * @return
+ */
+void deplace(Grille, Position, Position, Grille);
+
+/**
+ * Déplace le Sokoban sur la Grille
+ * @param Grille, Position, Position, Grille
+ * @return Position
+ */
 Position joue(Grille, Commande, Position, Grille);
+
+/**
+ * Teste si le joueur a gagné
+ * @param Grille, Position, Grille
+ * @return Boolean
+ */
 Boolean gagne(Grille, Position, Grille);
+
+/**
+ * Crée une grille et l'alloue
+ * @param integer, integer
+ * @return Grille
+ */
 Grille creerGrille(int, int);
+
+/**
+ * Copie la grille passé en paramètre et la renvoie
+ * @param Grille
+ * @return Grille
+ */
 Grille copieGrille(Grille);
 
 #endif
